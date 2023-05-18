689fae01c97b2ae0a96327a13e2edefb4a275ff5
SheepStar02 <wetmanroyale@gmail.com>
2023-05-17 22:41:34 -0400

    update standard values

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Param, Controller, Post, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -9,6 +9,13 @@ export class UserController {
   addUser(@Body() body) {
     console.log(body);
     return this.userService.addUser(body);
+  }
+
+  @Patch()
+  updateUser(@Body() body) {
+    console.log(body);
+    const { email, standard_values } = body;
+    return this.userService.updateUser(email, standard_values)
   }
 
   @Post('authenticate')
